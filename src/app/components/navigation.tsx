17cf14ca9f467e1fdefae67021370c4ffdb027a4
seven-renato <seven.renato@hotmail.com>
2024-12-04 02:25:08 -0300

    fix: never used variables to deploy

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Navigation() {
